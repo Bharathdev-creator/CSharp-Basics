56cedd4e2979c730ecc53698c77c0969b06af692
Bharathdev-creator <bharath.polavarapugit@gmail.com>
2025-09-03 20:19:51 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//name space is like a folder to organize the code in big projects its used to avoid
+//naming conflicts between classes.
+// public static void iteration1() means:
+// “This is a method that anyone can call (public), it belongs to the class itself and doesn’t need objects (static), it doesn’t return anything (void), and its name is iteration1.”
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(10 + 5);
+        // Identifiers.Run();
+        //Variables.Run();
+        // DataTypes.Run();
+        // ConstantsAndLiterals.Run();
+        //Operator.RelationalOpe();
+        //Conditionals.Statement1();
+        //Loops.Party();
+        //BCJR.Jumps();
+        //Sarrays.multdimen();
+        //Strings.stringbul();
+    }
+}
+
